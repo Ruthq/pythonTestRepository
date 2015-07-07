699d2a29c9546058d9d096c44f87066242960cfc
Ruthq <charispoch@gmail.com>
2015-07-07 08:56:31 -0700

    Added more necessary code

--- Scarlet.py
+++ Scarlet.py
@@ -1,12 +1,18 @@
 class Character : 
 
-	def __init__ (self, name, streangth, health,):
+	def __init__ (self, name, streangth, health, speed):
 		self.name = name
 		self.streangth = streangth
 		self.health = health
+		self.speed = speed
+	
+class Monster(Character) :
+	def __init__ (self, name, streangth, health, speed, poision, stealth)
+	Character.__init__(self, name, streangth, health, speed)
+	self.poision = poision
+	self.stealth = stealth
 
-class Explorer(Character) :
-	
-	
-class Monster :
-	pass
+class Player(Character) :
+	def __init__ (self, name, streangth, health, speed, inventory,)
+	Character.__init__(self, name, streangth, health, speed)
+	self.inventory = inventory
